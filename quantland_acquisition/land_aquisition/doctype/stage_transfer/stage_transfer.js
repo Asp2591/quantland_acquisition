951d74f974fb8568a320ae8ba75b671e3dcf6e97
Atharva <atharvapatkar@erpdata.in>
2025-08-29 17:33:11 +0530

    29/08/25 stage transfer and gut details change

--- quantland_acquisition/land_aquisition/doctype/stage_transfer/stage_transfer.js
+++ quantland_acquisition/land_aquisition/doctype/stage_transfer/stage_transfer.js
@@ -20,35 +20,37 @@ frappe.ui.form.on('Stage Transfer', {
 });
 
 frappe.ui.form.on('Stage Transfer Details', {
-    village_id: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (!frm.doc.prastav_id || !row.village_id) return;
-
-        frappe.db.get_doc("Prastav", frm.doc.prastav_id).then(prastav => {
-            let detail_row = (prastav.prastav_details || []).find(r =>
-                String(r.village_id).trim() === String(row.village_id).trim()
-            );
-
-            if (detail_row) {
-                row.village_name = detail_row.village_name;
-                row.gut_number = detail_row.gut_number;
-                row.total_area = detail_row.total_area;
-                row.affected_area = detail_row.affected_area;
-                row.aquisition_type = detail_row.aquisition_type;
-                row.landholder = detail_row.landholder;
-                row.land_type = detail_row.land_type;
-
-                frm.refresh_field('stage_transfer_details');
-            } else {
-                row.village_name = '';
-                row.gut_number = '';
-                row.total_area = 0;
-                row.affected_area = 0;
-                row.aquisition_type = '';
-                row.landholder = '';
-                row.land_type = '';
-                frm.refresh_field('stage_transfer_details');
-            }
-        });
+    gut_number: function(frm, cdt, cdn) {
+        auto_fill_by_gut(frm, cdt, cdn);
     }
 });
+
+function auto_fill_by_gut(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+
+    if (!frm.doc.prastav_id || !row.gut_number) return;
+
+    frappe.db.get_doc("Prastav", frm.doc.prastav_id).then(prastav => {
+        let detail_row = (prastav.prastav_details || []).find(r =>
+            String(r.gut_number).trim() === String(row.gut_number).trim()
+        );
+
+        if (detail_row) {
+            frappe.model.set_value(cdt, cdn, 'village_id', detail_row.village_id);
+            frappe.model.set_value(cdt, cdn, 'village_name', detail_row.village_name);
+            frappe.model.set_value(cdt, cdn, 'total_area', detail_row.total_area);
+            frappe.model.set_value(cdt, cdn, 'affected_area', detail_row.affected_area);
+            frappe.model.set_value(cdt, cdn, 'aquisition_type', detail_row.aquisition_type);
+            frappe.model.set_value(cdt, cdn, 'landholder', detail_row.landholder);
+            frappe.model.set_value(cdt, cdn, 'land_type', detail_row.land_type);
+        } else {
+            frappe.model.set_value(cdt, cdn, 'village_id', '');
+            frappe.model.set_value(cdt, cdn, 'village_name', '');
+            frappe.model.set_value(cdt, cdn, 'total_area', 0);
+            frappe.model.set_value(cdt, cdn, 'affected_area', 0);
+            frappe.model.set_value(cdt, cdn, 'aquisition_type', '');
+            frappe.model.set_value(cdt, cdn, 'landholder', '');
+            frappe.model.set_value(cdt, cdn, 'land_type', '');
+        }
+    });
+}
