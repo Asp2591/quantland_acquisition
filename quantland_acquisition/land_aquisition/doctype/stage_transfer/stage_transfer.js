a7918f47f4b429cbbdbd004ad1c56ddd09a4d2dc
Atharva <Administrator>
2025-09-19 13:04:44 +0530

    19/09/25 Stage transfer fetching

--- quantland_acquisition/land_aquisition/doctype/stage_transfer/stage_transfer.js
+++ quantland_acquisition/land_aquisition/doctype/stage_transfer/stage_transfer.js
@@ -1,3 +1,4 @@
+
 // Copyright (c) 2025, Quantbit Tech and contributors
 // For license information, please see license.txt
 
@@ -13,32 +14,68 @@ frappe.ui.form.on('Stage Transfer', {
             }));
         });
     },
+
     new_stage: function(frm) {
         if (!frm.doc.new_stage) return;
 
-        frm.doc.stage_transfer_details.forEach(row => {
+        (frm.doc.stage_transfer_details || []).forEach(row => {
             frappe.model.set_value(row.doctype, row.name, 'new_stage', frm.doc.new_stage);
         });
         frm.refresh_field('stage_transfer_details');
     },
+    
     gut_name: function(frm) {
     if (!frm.doc.gut_name) return;
 
-    frappe.db.get_value("Gut Details", frm.doc.gut_name, "current_stage")
+    frappe.db.get_value("Gut Details", frm.doc.gut_name, ["current_stage", "srno", "stage_date"])
         .then(r => {
             if (r && r.message) {
                 frm.set_value("current_stage", r.message.current_stage);
-                console.log("current_stage:", r.message.current_stage, "new_stage:", frm.doc.new_stage);
+                frm.set_value("srno", r.message.srno);
+                frm.set_value("date", r.message.stage_date);
+                console.log(r.message,"current_stage:", r.message.current_stage, 
+                            "srnno:", r.message.srno, 
+                            "date:", r.message.stage_date, 
+                            "new_stage:", frm.doc.new_stage);
             }
         });
-    }
+
+    (frm.doc.stage_transfer_details || []).forEach(row => {
+        frappe.model.set_value(row.doctype, row.name, 'gut_number', frm.doc.gut_name);
+    });
+    frm.refresh_field('stage_transfer_details');
+}
+
 });
 
 frappe.ui.form.on('Stage Transfer Details', {
-    gut_number: function(frm, cdt, cdn) {
-        auto_fill_by_gut(frm, cdt, cdn);
+    stage_transfer_details_add: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+
+        if (frm.doc.gut_name) {
+            frappe.model.set_value(cdt, cdn, 'gut_number', frm.doc.gut_name);
+
+            frappe.db.get_doc("Gut Details", frm.doc.gut_name).then(gut => {
+                if (gut) {
+                    frappe.model.set_value(cdt, cdn, 'village_id', gut.village_id || '');
+                    frappe.model.set_value(cdt, cdn, 'village_name', gut.village_name || '');
+                    frappe.model.set_value(cdt, cdn, 'total_area', gut.total_area || 0);
+                    frappe.model.set_value(cdt, cdn, 'affected_area', gut.affected_area || 0);
+                    frappe.model.set_value(cdt, cdn, 'acquisition_type', gut.aquisition_type || '');
+                    frappe.model.set_value(cdt, cdn, 'landholder', gut.landholder || '');
+                    frappe.model.set_value(cdt, cdn, 'land_type', gut.land_type || '');
+                    frappe.model.set_value(cdt, cdn, 'current_stage', gut.current_stage || '');
+                    frappe.model.set_value(cdt, cdn, '712_record', gut.record || '');
+                }
+            });
+        }
+
+        if (frm.doc.new_stage) {
+            frappe.model.set_value(cdt, cdn, 'new_stage', frm.doc.new_stage);
+        }
     }
 });
+
 
 function auto_fill_by_gut(frm, cdt, cdn) {
     let row = locals[cdt][cdn];
